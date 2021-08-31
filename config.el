34f0cff683ba2934ce8e9ff68e26b343f288482a
Erich Grunewald <erichgrunewald@gmail.com>
2021-08-31 16:08:35 +0200

    Ch ch ch changes

--- config.el
+++ config.el
@@ -1,59 +1,37 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
-
-
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets.
+;; identifying information, e.g. for gpg configuration, email clients, file
+;; templates and snippets.
 (setq user-full-name "Erich Grunewald"
       user-mail-address "erichgrunewald@gmail.com")
 
-;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
-;; are the three important ones:
-;;
-;; + `doom-font'
-;; + `doom-variable-pitch-font'
-;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;;
-;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
-;; font string. You generally only need these two:
-;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
-;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
+;; theme
 (setq doom-font (font-spec :family "Apercu" :size 16 :weight 'normal))
+(setq doom-theme 'doom-homage-black)
 
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-wilmersdorf)
+;; modeline
+(setq doom-modeline-buffer-file-name-style 'truncate-except-project)
 
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
-
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type t)
-
-;; Here are some additional functions/macros that could help you configure Doom:
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package!' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
+;; email
+(set-email-account!
+ "gmail"
+ '((mu4e-sent-folder       . "/[Gmail].Sent Mail")
+   (mu4e-drafts-folder     . "/[Gmail].Starred")
+   (mu4e-trash-folder      . "/[Gmail].Bin")
+   (mu4e-refile-folder     . "/INBOX")
+   (smtpmail-smtp-user     . "erichgrunewald@gmail.com"))
+ t)
+(setq mu4e-get-mail-command "mbsync gmail")
+(setq mu4e-compose-format-flowed t)
+;; don't need to run cleanup after indexing for gmail
+(setq mu4e-index-cleanup nil
+      mu4e-index-lazy-check t)
+;; this fixes some macos keychain issue.
+;; => https://github.com/zzamboni/dot-doom/blob/master/doom.org
+(after! auth-source (setq auth-sources (nreverse auth-sources)))
 
 ;; org
+(setq org-directory "~/org/")
 (map! "C-c C-g" 'org-forward-heading-same-level) ; C-c C-b is already back
 (map! "C-c C-x l" '+org/close-all-folds)
 (map! "C-c C-x L" '+org/open-all-folds)
@@ -62,61 +40,62 @@
       org-journal-time-prefix "* "
       org-journal-date-format "%a, %Y-%m-%d"
       org-journal-file-format "%Y-%m-%d.org")
-(setq org-capture-templates
-      '(("t" "Todo" entry
-         (file+headline +org-capture-todo-file "Inbox")
-         "* [ ] %?" :prepend t)
-        ("s" "Someday todo" entry
-         (file+headline "~/org/somedaymaybe.org" "Someday / Maybe")
-         "* SOMEDAY %?")
-        ("m" "Maybe todo" entry
-         (file+headline "~/org/somedaymaybe.org" "Someday / Maybe")
-         "* MAYBE %?")
-        ("n" "Note" entry
-         (file+headline +org-capture-notes-file "Inbox")
-         "* %u %?\n%i" :prepend t)
+(after! org
+  (setq org-capture-templates
+        '(("t" "Todo" entry
+           (file+headline +org-capture-todo-file "Inbox")
+           "* [ ] %?" :prepend t)
+          ("s" "Someday todo" entry
+           (file+headline "~/org/somedaymaybe.org" "Someday / Maybe")
+           "* SOMEDAY %?")
+          ("m" "Maybe todo" entry
+           (file+headline "~/org/somedaymaybe.org" "Someday / Maybe")
+           "* MAYBE %?")
+          ("n" "Note" entry
+           (file+headline +org-capture-notes-file "Inbox")
+           "* %u %?\n%i" :prepend t)
 
-        ("r" "Templates for reviews")
-        ("rd" "Daily review" entry
-         (file+headline "~/org/reviews.org" "Reviews")
-         (file "~/org/templates/daily-review-template.org"))
-        ("rw" "Weekly review" entry
-         (file+headline "~/org/reviews.org" "Reviews")
-         (file "~/org/templates/weekly-review-template.org"))
-        ("rm" "Monthly review" entry
-         (file+headline "~/org/reviews.org" "Reviews")
-         (file "~/org/templates/monthly-review-template.org"))
+          ("r" "Templates for reviews")
+          ("rd" "Daily review" entry
+           (file+headline "~/org/reviews.org" "Reviews")
+           (file "~/org/templates/daily-review-template.org"))
+          ("rw" "Weekly review" entry
+           (file+headline "~/org/reviews.org" "Reviews")
+           (file "~/org/templates/weekly-review-template.org"))
+          ("rm" "Monthly review" entry
+           (file+headline "~/org/reviews.org" "Reviews")
+           (file "~/org/templates/monthly-review-template.org"))
 
-        ;; Will use {project-root}/{todo,notes,changelog}.org, unless a
-        ;; {todo,notes,changelog}.org file is found in a parent directory.
-        ;; Uses the basename from `+org-capture-todo-file',
-        ;; `+org-capture-changelog-file' and `+org-capture-notes-file'.
-        ("p" "Templates for projects")
-        ("pt" "Project-local todo" entry  ; {project-root}/todo.org
-         (file+headline +org-capture-project-todo-file "Inbox")
-         "* TODO %?" :prepend t)
-        ("pn" "Project-local notes" entry  ; {project-root}/notes.org
-         (file+headline +org-capture-project-notes-file "Inbox")
-         "* %U %?" :prepend t)
-        ("pc" "Project-local changelog" entry  ; {project-root}/changelog.org
-         (file+headline +org-capture-project-changelog-file "Unreleased")
-         "* %U %?" :prepend t)
+          ;; Will use {project-root}/{todo,notes,changelog}.org, unless a
+          ;; {todo,notes,changelog}.org file is found in a parent directory.
+          ;; Uses the basename from `+org-capture-todo-file',
+          ;; `+org-capture-changelog-file' and `+org-capture-notes-file'.
+          ("p" "Templates for projects")
+          ("pt" "Project-local todo" entry  ; {project-root}/todo.org
+           (file+headline +org-capture-project-todo-file "Inbox")
+           "* TODO %?" :prepend t)
+          ("pn" "Project-local notes" entry  ; {project-root}/notes.org
+           (file+headline +org-capture-project-notes-file "Inbox")
+           "* %U %?" :prepend t)
+          ("pc" "Project-local changelog" entry  ; {project-root}/changelog.org
+           (file+headline +org-capture-project-changelog-file "Unreleased")
+           "* %U %?" :prepend t)
 
-        ;; Will use {org-directory}/{+org-capture-projects-file} and store
-        ;; these under {ProjectName}/{Tasks,Notes,Changelog} headings. They
-        ;; support `:parents' to specify what headings to put them under, e.g.
-        ;; :parents ("Projects")
-        ("o" "Centralized templates for projects")
-        ("ot" "Project todo" entry
-         (function +org-capture-central-project-todo-file)
-         "* TODO %?" :heading "Tasks" :prepend nil)
-        ("on" "Project notes" entry
-         (function +org-capture-central-project-notes-file)
-         "* %U %?" :heading "Notes" :prepend t)
-        ("oc" "Project changelog" entry
-         (function +org-capture-central-project-changelog-file)
-         "* %U %?" :heading "Changelog" :prepend t)
-        ))
+          ;; Will use {org-directory}/{+org-capture-projects-file} and store
+          ;; these under {ProjectName}/{Tasks,Notes,Changelog} headings. They
+          ;; support `:parents' to specify what headings to put them under, e.g.
+          ;; :parents ("Projects")
+          ("o" "Centralized templates for projects")
+          ("ot" "Project todo" entry
+           (function +org-capture-central-project-todo-file)
+           "* TODO %?" :heading "Tasks" :prepend nil)
+          ("on" "Project notes" entry
+           (function +org-capture-central-project-notes-file)
+           "* %U %?" :heading "Notes" :prepend t)
+          ("oc" "Project changelog" entry
+           (function +org-capture-central-project-changelog-file)
+           "* %U %?" :heading "Changelog" :prepend t)
+          )))
 
 (defun erich/org-to-clipboard-as-markdown ()
   "Export marked org text to Markdown and put it in clipboard.
@@ -133,9 +112,14 @@ I found this somewhere but cannot locate the source now."
 
 ;; org-roam
 (setq org-roam-directory (file-truename "~/org-roam"))
+(setq +org-roam-open-buffer-on-find-file nil)
 
 ;; projectile
-(setq projectile-project-search-path "~/Documents/code/")
+(setq projectile-project-search-path
+      (append (cdddr (directory-files "~/Documents/projects/programming" t))
+              (cdddr (directory-files "~/Documents/projects" t))))
+(setq projectile-globally-ignored-file-suffixes
+      '(".png" ".jpg" ".gif" ".pdf" ".ipynb"))
 
 ;; mit/gnu scheme
 (setq geiser-active-implementations '(mit))
@@ -147,21 +131,46 @@ I found this somewhere but cannot locate the source now."
 (setq lsp-julia-package-dir nil)
 (setq lsp-enable-folding t)
 (setq lsp-julia-default-environment "~/.julia/environments/v1.6")
-;; (after! julia-repl (set-popup-rule! "^\\*julia.*\\*$" :ignore t))
 
 ;; python
 (map! :desc "Show the Poetry menu." "C-c y" 'poetry)
 
-;; general utility
+;; editor
+(setq-default delete-by-moving-to-trash t
+              fill-column 120)
+(setq display-line-numbers-type t
+      scroll-margin 2)
+(add-to-list 'default-frame-alist '(fullscreen . maximized)) ; start maximized
+(global-display-fill-column-indicator-mode) ; show fill col by default
+(+global-word-wrap-mode +1) ; turn on soft word wrap almost everywhere
+(after! which-key (setq which-key-idle-delay 0.1)) ; open popup more quickly
+
+;; associate some file extensions with modes
+(add-to-list 'auto-mode-alist '("\\.njk\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tidal\\'" . haskell-mode))
+
+;; general utility keybinds
 (map! "C-c b" 'pop-global-mark) ; return to last mark
 (map! "C-S-k" 'kill-visual-line) ; note sure about this one, delete?
 (map! "C-c C-w" 'fixup-whitespace) ; removes extra whitespace around cursor
 (map! "C-M-q" 'indent-pp-sexp) ; correctly indents sexp starting after cursor
 (map! "C-x C-a" 'ace-jump-mode) ; jump to char, line or word
 (map! "C-x C-M-a" 'ace-jump-mode-pop-mark) ; return to previous ace position
+(map! "C-i" 'other-window) ; select next window in cyclic ordering
 
-;; layout
-(add-to-list 'default-frame-alist '(fullscreen . maximized)) ; start maximized
+;; lisp editing
+(map! (:prefix "C-m"
+       "k" 'sp-kill-sexp
+       "b f" 'sp-forward-barf-sexp
+       "b b" 'sp-backward-barf-sexp
+       "s f" 'sp-forward-slurp-sexp
+       "s b" 'sp-backward-slurp-sexp
+       "r" 'sp-raise-sexp
+       "c" 'sp-convolute-sexp
+       "s" 'sp-split-sexp
+       "j" 'sp-join-sexp
+       "w r" 'sp-wrap-round
+       "w c" 'sp-wrap-curly))
 
 (defun erich/split-windows ()
   "Splits windows my way.
@@ -179,7 +188,7 @@ I found this somewhere but cannot locate the source now."
            (--any? (s-contains? ".jl" it)
                    (projectile-project-files (projectile-project-root))))
       (+eval/open-repl-same-window)
-    (shell))
+    (vterm))
   ;; Never open any buffer in window with shell.
   (set-window-dedicated-p (nth 1 (window-list)) t)
   ;; Go to original window.
@@ -244,7 +253,10 @@ I found this somewhere but cannot locate the source now."
               (date (read-string "Date (yyyy-mm-dd): ")))
           (let ((file-path
                  (concat root-dir "posts/"
-                         (s-dashed-words (s-replace "'" "" title)) ".md")))
+                         (s-dashed-words
+                          (s-replace-regexp "[[:nonascii:]]" ""
+                                            (s-replace "'" "" title)))
+                         ".md")))
             (progn (write-region (concat
                                   "---\n"
                                   "layout: layouts/post.njk\n"
@@ -258,6 +270,7 @@ I found this somewhere but cannot locate the source now."
                    (goto-char (point-min))
                    (message "Done!"))))
       (message "Not in /blog directory."))))
+
 
 (defun erich/blog-post-fix-urls ()
   "Fixes all URLs, both to the blog itself & to images (points them to the /img
--- init.el
+++ init.el
@@ -54,7 +54,7 @@
        ;;(evil +everywhere); come to the dark side, we have cookies
        file-templates    ; auto-snippets for empty files
        fold              ; (nigh) universal code folding
-       (format +onsave)  ; automated prettiness
+       format  ; automated prettiness
        ;;god               ; run Emacs commands without modifier keys
        ;;lispy             ; vim for lisp, for people who don't like vim
        multiple-cursors  ; editing in many places at once
@@ -75,7 +75,7 @@
        ;;eshell            ; the elisp shell that works everywhere
        ;;shell             ; simple shell REPL for Emacs
        ;;term              ; basic terminal emulator for Emacs
-       ;;vterm             ; the best terminal emulation in
+       vterm             ; the best terminal emulation in
        :checkers
        syntax              ; tasing you for every semicolon you forget
        ;;spell             ; tasing you for misspelling mispelling
@@ -85,7 +85,7 @@
        ;;ansible
        ;;debugger          ; FIXME stepping through code, to help you add bugs
        ;;direnv
-       ;;docker
+       docker
        ;;editorconfig      ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
@@ -99,9 +99,9 @@
        ;;prodigy           ; FIXME managing external services & code builders
        rgb               ; creating color strings
        ;;taskrunner        ; taskrunner for all your projects
-       ;;terraform         ; infrastructure as code
+       terraform         ; infrastructure as code
        ;;tmux              ; an API for interacting with tmux
-       ;;upload            ; map local to remote projects via ssh/ftp
+       upload            ; map local to remote projects via ssh/ftp
 
        :os
        (:if IS-MAC macos)  ; improve compatibility with macOS
@@ -150,7 +150,7 @@
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
-       python            ; beautiful is better than ugly
+       (python +lsp +pyright)            ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
@@ -169,7 +169,7 @@
        yaml              ; JSON, but readable
 
        :email
-       ;;(mu4e +gmail)
+       (mu4e +gmail)
        ;;notmuch
        ;;(wanderlust +gmail)
 
